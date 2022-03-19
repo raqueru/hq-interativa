87bdb6cd2f2f15bd56dfa9962ae99f5b13e8673c
Raks <raquelolho@hotmail.com>
2022-03-19 12:23:01 -0300

    escalando video

--- hqinterativa/Assets/scripts/Frame.cs
+++ hqinterativa/Assets/scripts/Frame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class Frame : MonoBehaviour
 {
@@ -10,33 +11,47 @@ public class Frame : MonoBehaviour
     public bool animated;
     public int currentAnimation;
     private VideoPlayer videoPlayer;
+    private RawImage image;
+    RenderTexture renderTexture;
 
     void Awake()
     {
 
+
         if (animated)
         {
+            image = GetComponentInParent<RawImage>();
 
+            if (renderTexture == null)
+                renderTexture = new RenderTexture(350, 300, 24);
             GameObject camera = GameObject.Find("Main Camera");
 
 
             videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
 
+            image.texture = renderTexture;
 
-            videoPlayer.playOnAwake = true;
 
-            videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+            videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
 
+            videoPlayer.targetTexture = renderTexture;
+
+
+            videoPlayer.aspectRatio = VideoAspectRatio.FitInside;
             videoPlayer.clip = videos[currentAnimation];
             videoPlayer.targetCameraAlpha = 0.5F;
 
 
-
             videoPlayer.frame = 0;
+
 
             videoPlayer.isLooping = false;
 
             videoPlayer.loopPointReached += EndReached;
+
+            Debug.Log(videoPlayer.texture);
+
+
         }
 
 
